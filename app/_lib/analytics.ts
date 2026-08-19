a7e7ae8ec9fa59d66f67afddd7568c4a5302c059
Zulqarnain Mukhtar <xulkernain@gmail.com>
2026-08-20 02:09:31 +0500

    Add Google Analytics tracking

---
+++ app/_lib/analytics.ts
@@ -0,0 +1,1 @@
+export const googleAnalyticsId = "G-8QE8L9Q5PL";
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import Script from "next/script";
+import { googleAnalyticsId } from "./_lib/analytics";
 import { siteDescription, siteName, siteUrl } from "./_lib/seo";
 import "./globals.css";
 
@@ -28,7 +30,21 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className="h-full antialiased">
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${googleAnalyticsId}');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
